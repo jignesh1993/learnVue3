4b3fc33d90bbd2483f7ee7a07400c7e3cc2c3343
Jignesh <jignesh.mahesh07@gmail.com>
2022-06-08 15:05:41 +0530

    #23 Complete Form Part - 2

--- src/components/SimpleForm.vue
+++ src/components/SimpleForm.vue
@@ -1,12 +1,36 @@
 <template>
 <div class="center">
     <h2>Simple Form</h2>
+    <p>{{form}}</p>
     <form>
         <label>Email:</label>
         <input type="text" placeholder="Enter Email" v-model="form.email" /> <br /><br />
 
         <label>Password:</label>
         <input type="password" placeholder="Enter Password" v-model="form.password" /> <br /><br />
+
+        <select v-model="form.country">
+            <option disabled value="" selected="selected">Please select country</option>
+            <option>India</option>
+            <option>China</option>
+            <option>USA</option>
+        </select> <br /> <br />
+
+        <h3>Technology :</h3>
+        <label for="django">Django</label>
+        <input type="checkbox" value="django" id="django" v-model="form.technology" />
+
+        <label for="python">python</label>
+        <input type="checkbox" value="python" id="python" v-model="form.technology" />
+        <br /><br />
+
+        <h3>Gender :</h3>
+        <label for="male">Male</label>
+        <input type="radio" value="male" id="male" name="gender" v-model="form.gender" />
+
+        <label for="female">Female</label>
+        <input type="radio" value="female" id="female" name="gender" v-model="form.gender" />
+        <br /><br />
 
         <button type="button" v-on:click="login">Login</button>
     </form>
@@ -20,14 +44,16 @@ export default {
         return {
             form: {
                 email: '',
-                password: ''
+                password: '',
+                country: '',
+                technology: [],
+                gender: ''
             }
         }
     },
     methods: {
         login() {
-            console.log('<<-- this.form.email -->> ', this.form.email);
-            console.log('<<-- this.form.password -->>', this.form.password);
+            console.log('<<-- this.form-->> ', this.form);
         }
     }
 }
