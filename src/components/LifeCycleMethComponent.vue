6f759931ba07b1a3c45edfed68b37146478ec401
jignesh1993 <jignesh.mahesh07@gmail.com>
2022-08-29 18:24:21 +0530

    #37 beforeUnmount and unmounted

--- src/components/LifeCycleMethComponent.vue
+++ src/components/LifeCycleMethComponent.vue
@@ -1,26 +1,35 @@
 <template>
-    <h1>Life Cycle Method {{name}}</h1>
-    <h1>beforeUpdate and updated</h1>
-    <h1 ref="count">{{counter}}</h1>
-    <button v-on:click="counter=counter+1">Update Counter</button>
+<h1>Life Cycle Method {{ name }}</h1>
+<h1>beforeUpdate and updated</h1>
+<h1 ref="count">{{ counter }}</h1>
+<button v-on:click="counter = counter + 1">Update Counter</button>
+
+<br /><br /><br />
+<button v-on:click="show=!show">toggle</button>
+<UserComponent2 v-if="show" />
 </template>
 
 <script>
+import UserComponent2 from "./UserComponent2";
 export default {
-    name: 'LifeCycleMethComponent',
-    data(){
-        return{
-            name: 'John',
-            counter: 0
-        }
+    name: "LifeCycleMethComponent",
+    components : {
+        UserComponent2,
+    },
+    data() {
+        return {
+            name: "John",
+            counter: 0,
+            show: true,
+        };
     },
     beforeCreate() {
-        this.name = 'Jane1'
-        alert('Before Create')
+        this.name = "Jane1";
+        alert("Before Create");
     },
-    created(){
-        this.name = 'Jane2'
-        alert('After Created')
+    created() {
+        this.name = "Jane2";
+        alert("After Created");
     },
     beforeMount() {
         // console.log('beforeMount ==>>', this.$el);
@@ -29,16 +38,22 @@ export default {
         // console.log('Mounted ==>>', this.$el);
     },
     beforeUpdate() {
-        console.log('beforeUpdate ==>>', this.$refs['count'].textContent);
+        console.log("beforeUpdate ==>>", this.$refs["count"].textContent);
     },
     updated() {
-        console.log('afterUpdate ==>>', this.$refs['count'].textContent);
+        console.log("afterUpdate ==>>", this.$refs["count"].textContent);
+    },
+    beforeUnmount(){
+        console.log('beforeUnmount ==>>');
+    },
+    unmounted() {
+        console.log('unmounted ==>>');
     }
-}
+};
 </script>
 
 <style scoped>
 h1 {
-  color: orange;
+    color: orange;
 }
 </style>
---
+++ src/components/UserComponent2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="user">
+       <h1>User component 2</h1>
+    </div>
+    </template>
+    
+    <script>
+    export default {
+        name: 'UserComponent2',
+    }
+    </script>
+    
+    <style scoped>
+    </style>
+    
